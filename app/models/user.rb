4aaddd1917584a21d5c86c4f70fd50d91350ad9b
Fernando Espinosa <fer@ferdev.com>
2013-03-15 10:32:04 +0100

    Makes public a method in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -156,11 +156,4 @@ class User < ActiveRecord::Base
   end
   private :generate_token
 
-  def update_last_login
-    self.last_login                             = Time.now
-    self.six_months_since_last_login_alert_sent = false
-    self.save if persisted?
-  end
-  private :update_last_login
-
 end
