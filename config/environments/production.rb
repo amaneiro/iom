7227e7689cc7e73299e39e6a2dfd68107265eebf
Andrés Maneiro <amaneiro@icarto.es>
2013-07-15 21:33:23 +0200

    Revert last commit, but use memory_store instead of memcache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Iom::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
@@ -25,7 +25,7 @@ Iom::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store
+  config.cache_store = :memory_store, :size => 64.megabytes
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
