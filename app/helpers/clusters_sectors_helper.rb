3e06750123e670cbaf21b5105067e289a3e778f6
Andrés Maneiro <amaneiro@icarto.es>
2013-11-22 19:13:25 +0100

    wip #4 - translate sector view

--- app/helpers/clusters_sectors_helper.rb
+++ app/helpers/clusters_sectors_helper.rb
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 module ClustersSectorsHelper
   def clusters_sectors_projects_list_subtitle
     if @filter_by_location
-      pluralize(@cluster_sector_projects_count, "#{@data.name} PROJECT", "#{@data.name} PROJECTS") + " in #{@location_name}"
+      pluralize(@cluster_sector_projects_count, "PROYECTO DE #{@data.name}", "PROYECTOS DE #{@data.name}") + " EN #{@location_name}"
     else
        location = if @site.navigate_by_country?
-         pluralize(@data.total_countries(@site), 'country', 'countries')
+         pluralize(@data.total_countries(@site), 'país', 'países')
        else
          pluralize(@data.total_regions(@site), @site.word_for_regions.singularize, @site.word_for_regions)
        end
-       "#{pluralize(@cluster_sector_projects_count, 'PROJECT', 'PROJECTS')}  in #{location}"
+       "#{pluralize(@cluster_sector_projects_count, 'PROYECTO', 'PROYECTOS')}  EN #{location}"
     end
   end
 
