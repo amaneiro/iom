87fe94d830cf85adc013ea03e1baa22bde9f5bb1
Andrés Maneiro <amaneiro@icarto.es>
2013-11-22 19:13:37 +0100

    wip #4 - translate region view

--- app/helpers/georegion_helper.rb
+++ app/helpers/georegion_helper.rb
@@ -1,9 +1,9 @@
 module GeoregionHelper
   def georegion_projects_list_subtitle
     if @filter_by_category
-      pluralize(@georegion_projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + " in #{@area.name.upcase}"
+      pluralize(@georegion_projects_count, "PROYECTO DE #{@category_name}", "PROYECTOS DE #{@category_name}") + " EN #{@area.name.upcase}"
     else
-      "#{pluralize(@georegion_projects_count, 'PROJECT', 'PROJECTS')} IN #{@area.name.upcase}"
+      "#{pluralize(@georegion_projects_count, 'PROYECTO', 'PROYECTOS')} EN #{@area.name.upcase}"
     end
   end
 
